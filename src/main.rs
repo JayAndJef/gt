c37395d495179a209117a01fed3c338ae3a3d5d0
JayAndJef <109392310+JayAndJef@users.noreply.github.com>
2024-01-26 20:15:45 -0800

    add database config

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,22 @@ pub mod handler;
 pub mod cli;
 
 use cli::{Goto, CommandEnum};
+use rusqlite::Connection;
+
+const DB_PATH: &str = "~/.config/gt/db.db3";
 
 fn main() {
     let goto: Goto = argh::from_env();
+
+    let db = Connection::open(DB_PATH).expect("failed to initialize database");
+    db.execute(
+        "CREATE TABLE IF NOT EXISTS bookmarks (
+            name TEXT NOT NULL,
+            path TEXT NOT NULL,
+            description TEXT
+        )",
+        (),
+    ).expect("failed to create database table");
 
     match goto.nested {
         CommandEnum::Add(addargs) => todo!(),
